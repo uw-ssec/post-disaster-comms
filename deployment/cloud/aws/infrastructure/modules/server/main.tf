929604d5f3192cf78f249bc45d2986880397195f
nikiburggraf <nikib@uw.edu>
2024-09-26 17:07:51 -0700

    feat: Set up ALB & DNS

--- deployment/cloud/aws/infrastructure/modules/server/main.tf
+++ deployment/cloud/aws/infrastructure/modules/server/main.tf
@@ -129,8 +129,6 @@ resource "aws_security_group" "this" {
   }
 }
 
-// launch template -- userdata tbd for now
-
 resource "aws_eip" "this" {
   domain = "vpc"
 }
@@ -206,4 +204,37 @@ resource "aws_autoscaling_schedule" "scale_down" {
   max_size = 1
   recurrence = "0 1 * * MON-FRI"
   autoscaling_group_name = aws_autoscaling_group.this.name
+}
+
+// ALB and target group
+resource "aws_lb" "this" {
+  name = "${var.resource_prefix}-lb"
+  internal = false
+  load_balancer_type = "application"
+  security_groups = [aws_security_group.this.id]
+  subnets = module.vpc.public_subnets
+}
+
+resource "aws_lb_target_group" "this" {
+  name = "${var.resource_prefix}-tg"
+  port = 80
+  protocol = "HTTP"
+  vpc_id = module.vpc.vpc_id
+  target_type = "instance"
+}
+
+resource "aws_lb_listener" "this" {
+  load_balancer_arn = aws_lb.this.arn
+  port = 80
+  protocol = "HTTP"
+
+  default_action {
+    type = "forward"
+    target_group_arn = aws_lb_target_group.this.arn
+  }
+}
+
+resource "aws_autoscaling_attachment" "this" {
+  autoscaling_group_name = aws_autoscaling_group.this.name
+  lb_target_group_arn = aws_lb_target_group.this.arn
 }
